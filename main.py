b8a22ff3872a6985723eb2feb9bd54486e697078
Ángel López <angelo06@ucm.es>
2022-09-12 11:07:51 +0200

    El señor burns
    Y sus locuras

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
+def intengra_mc(fun, a, b, num_puntos = 10000):
+    integral = 0
+    Ndebajo = 0; 
+    M = 0; 
+     
+    integral = (Ndebajo / num_puntos) * (b - a) * M
+    
+    return integral
+
+def cuadrado(x):
+    return x * x
+
 def main() :
-    print("hello, world\n")
+    print(cuadrado(2), "\n")
 
 
 main()
