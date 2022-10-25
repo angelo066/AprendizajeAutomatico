7f7767973b881743e5e1f8c7f3c92c01484f620f
JosedaMachine <jrave@ucm.es>
2022-10-25 16:39:53 +0200

    Hecha la memoria

--- P4/p4/multi_class.py
+++ P4/p4/multi_class.py
@@ -111,29 +111,6 @@ def predictOneVsAll(all_theta, X):
 # NN
 #
 def predict(theta1, theta2, X):
-    """
-    Predict the label of an input given a trained neural network.
-
-    Parameters
-    ----------
-    theta1 : array_like
-        Weights for the first layer in the neural network.
-        It has shape (2nd hidden layer size x input size)
-
-    theta2: array_like
-        Weights for the second layer in the neural network. 
-        It has shape (output layer size x 2nd hidden layer size)
-
-    X : array_like
-        The image inputs having shape (number of examples x image dimensions).
-
-    Return 
-    ------
-    p : array_like
-        Predictions vector containing the predicted label for each example.
-        It has a length equal to the number of examples.
-    """
-    
     m = X.shape[0]
     #Input matrix
     X1s = np.c_[np.ones(m), X]
